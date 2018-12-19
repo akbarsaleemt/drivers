2dac00e4b7db1193549415a6e30cc0d97c4616fe
akbar <akbarsaleem402@gmail.com>
2018-12-19 17:37:41 +0530

    drivers

---
+++ basicdriver/basicdriv.c
@@ -0,0 +1,23 @@
+#include<linux/init.h>
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/version.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("AKBAR");
+MODULE_DESCRIPTION("BASIC DRIVER");
+MODULE_VERSION("0.1");
+
+  static int my_init(void)
+   {  
+     printk(KERN_ALERT "hello world\n");
+     return 0;
+   }
+  void  my_exit(void)
+    {
+      printk(KERN_ALERT"exit");
+      return;
+    }
+module_init(my_init);
+module_exit(my_exit);
+
